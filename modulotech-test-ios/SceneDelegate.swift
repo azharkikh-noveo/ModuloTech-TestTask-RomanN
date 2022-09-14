34cebb260850c9cabd67ab182cee9b372a2c5b23
whutao <nabroman42@icloud.com>
2022-09-15 00:48:05 +0300

    Add empty project.

---
+++ modulotech-test-ios/SceneDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  SceneDelegate.swift
+//  modulotech-test-ios
+//
+//  Created by Roman Nabiullin on 15.09.2022.
+//
+
+import UIKit
+
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .white
+        window?.rootViewController = UIViewController(nibName: nil, bundle: nil)
+        window?.makeKeyAndVisible()
+        
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    
+    }
+
+}
